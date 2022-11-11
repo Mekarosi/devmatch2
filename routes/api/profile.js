fb3c9d04947ddb9b80fa8effa3d129aaf7c112c6
Mekarosi <mekarosi_assa@yahoo.com>
2022-11-11 11:49:34 -0800

    Created route to get users github repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const request = require('request')
+const config = require('config')
 const router = express.Router()
 const auth = require('../../middleware/auth')
 const { check, validationResult } =
@@ -332,5 +334,33 @@ router.put('/education', [ auth,
      }
  })
 
+// @route  GET api/profile/github/:username
+// @desc   Get users repos from Github
+// @access Public
+
+router.get('/github/:username', async (req, res) => {
+   try {
+  
+    const options = {
+        uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+        method: 'GET',
+        headers: { 'user-agent': 'node.js' }
+    }
+
+    request(options, (error, response, body) => {
+        if(error) console.error(error)
+
+        if(response.statusCode != 200 ){
+           return res.status(404).json({ msg: 'No github profile found' })
+        }
+
+        res.json(JSON.parse(body))
+    })
+
+   } catch(err){
+    console.error(err.message)
+    res.status(500).send('Server Error')
+   }
+})
 
 module.exports = router
