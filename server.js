1ce3456a2133190aa24652def6413c521f93acc5
Mekarosi <mekarosi_assa@yahoo.com>
2022-12-02 12:09:28 -0800

    Added cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const connectDB = require('./config/db')
 
 const app = express()
+app.use(cors())
 
 // Connect Database
 connectDB()
